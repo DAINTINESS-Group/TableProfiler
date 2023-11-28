48b5252d37076c0ffd0cb3817e13e2d20e3524a8
siavasiatras <39170710+siavasiatras@users.noreply.github.com>
2023-11-28 20:26:34 +0200

    Add files via upload

---
+++ Resources/dwh_my_columns_extensions.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dwh
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `my_columns_extensions`
+--
+
+DROP TABLE IF EXISTS `my_columns_extensions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `my_columns_extensions` (
+  `TABLE_CATALOG` varchar(64) NOT NULL,
+  `TABLE_SCHEMA` varchar(64) NOT NULL,
+  `TABLE_NAME` varchar(64) NOT NULL,
+  `COLUMN_NAME` varchar(64) DEFAULT NULL,
+  `ENGINE_ATTRIBUTE` json DEFAULT NULL,
+  `SECONDARY_ENGINE_ATTRIBUTE` json DEFAULT NULL,
+  UNIQUE KEY `UK_columns_extensions` (`TABLE_CATALOG`,`TABLE_SCHEMA`,`TABLE_NAME`),
+  CONSTRAINT `FK_columns_extensions` FOREIGN KEY (`TABLE_CATALOG`, `TABLE_SCHEMA`, `TABLE_NAME`) REFERENCES `my_sample_tables` (`TABLE_CATALOG`, `TABLE_SCHEMA`, `TABLE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `my_columns_extensions`
+--
+
+LOCK TABLES `my_columns_extensions` WRITE;
+/*!40000 ALTER TABLE `my_columns_extensions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `my_columns_extensions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-28 20:22:45
